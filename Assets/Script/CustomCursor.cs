d324d4d5aa3607798a5c9d7142c9468c4702eb42
taha erel <tahaerelll@gmail.com>
2024-07-28 18:16:04 +0300

    Cursor added

---
+++ Assets/Script/CustomCursor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CustomCursor : MonoBehaviour
+{
+
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    void OnMouseExit()
+    {
+        // Pass 'null' to the texture parameter to use the default system cursor.
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+}
